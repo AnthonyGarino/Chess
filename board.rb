e821003ad01eda3796593d5da5c4a9908193e30c
Anthony Garino <Anthonygarino@utexas.edu>
2019-07-23 09:59:58 -0500

    Test

---
+++ board.rb
@@ -0,0 +1,9 @@
+class Board 
+
+    def initialize
+        @board = Array.new(8){Array.new(8)}
+    end 
+
+end
+
+
---
+++ chess.rb
@@ -0,0 +1,23 @@
+Phase 1
+
+# 1. create Board that holds a 2 dimensional array. 
+    a. each square on the board contains either a Piece or a NullPiece (NullPiece will inherit from Piece)
+    b. create an empty Piece class as a placeholder
+    c. write an initialize method so we can setup the board with instances of Piece, where pieces should be, and nil where NullPiece should be.
+    d. make a move_piece(start_pos, end_pos) method.
+        a. update the 2D grid and also the moved piece's' position
+        b. raise an exception if:
+            a. there is no piece at start_pos or
+            b. the piece cannot move to end_pos.
+
+2. TEST DIS BOY
+
+
+git config --global alias.c commit
+git config --global alias.s status
+git config --global alias.a add
+git config --global alias.p push
+git config --global alias.cam commit add -m
+
+git cam "Anthony"
+
